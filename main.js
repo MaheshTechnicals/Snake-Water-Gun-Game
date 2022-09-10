cc7b2fc649f46201aa46ccb1e49f28e86b8aa124
msvarma10001@gmail.com <msvarma10001@gmail.com>
2022-09-10 21:37:43 +0530

    Sound effects added

--- main.js
+++ main.js
@@ -11,8 +11,6 @@ let a = ["S", "G", "W"]
 
 let computer = a[Math.floor(Math.random() * a.length)]
 
-console.log(computer)
-
 
 btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
@@ -25,26 +23,28 @@ btns.forEach((btn) => {
       left.src = "charecters/s.png"
       right.src = "charecters/g.png"
       post.innerHTML = `<h3 class="red" >😔 YOU LOSS</h3>`
-
+      play("sounds/L.mp3")
     }
 
     else if (you.contains("S") && computer == "W") {
       left.src = "charecters/s.png"
       right.src = "charecters/w.png"
       post.innerText = "☺ YOU WIN"
-
+      play("sounds/win.mp3")
     }
 
     else if (you.contains("G") && computer == "S") {
       left.src = "charecters/g.png"
       right.src = "charecters/s.png"
       post.innerHTML = `😔 YOU LOS`
+      play("sounds/L.mp3")
     }
 
     else if (you.contains("G") && computer == "W") {
       left.src = "charecters/g.png"
       right.src = "charecters/w.png"
       post.innerHTML = `😔 YOU LOS`
+      play("sounds/L.mp3")
     }
 
 
@@ -52,14 +52,14 @@ btns.forEach((btn) => {
       left.src = "charecters/w.png"
       right.src = "charecters/s.png"
       post.innerText = "☺ YOU WIN"
-
+      play("sounds/win.mp3")
     }
 
     else if (you.contains("W") && computer == "G") {
       left.src = "charecters/w.png"
       right.src = "charecters/g.png"
       post.innerText = "☺ YOU WIN"
-
+      play("sounds/win.mp3")
     }
 
 
@@ -68,21 +68,21 @@ btns.forEach((btn) => {
       left.src = "charecters/s.png"
       right.src = "charecters/s.png"
       post.innerText = "😲 NOBODY WINS"
-
+      play("sounds/L.mp3")
     }
 
     else if (you.value = ("G") == computer) {
       left.src = "charecters/g.png"
       right.src = "charecters/g.png"
       post.innerText = "😲 NOBODY WINS"
-
+      play("sounds/L.mp3")
     }
 
     else if (you.value = ("W") == computer) {
       left.src = "charecters/w.png"
       right.src = "charecters/w.png"
       post.innerText = "😲 NOBODY WINS"
-
+      play("sounds/L.mp3")
     }
 
     ans.className = "out"
@@ -99,3 +99,10 @@ btns.forEach((btn) => {
 restart.addEventListener("click", () => {
   location.reload()
 })
+
+
+
+let play = (file) => {
+  let audio = new Audio(file)
+  audio.play()
+}
