879f77ba9fae21864cb814d6b0394aec438dc172
msvarma10001@gmail.com <msvarma10001@gmail.com>
2022-09-10 16:43:06 +0530

    Reload button added and some more importants

--- main.js
+++ main.js
@@ -1,42 +1,72 @@
-let a = ["S", "G", "W"]
-
-let you = prompt("Enter S, W or G....").toUpperCase()
-
-let computer = a[Math.floor(Math.random() * a.length)]
+let btns = document.querySelectorAll("img")
+let restart = document.querySelector("button")
 
 
 
-if (you == "S" && computer == "G") {
-  console.log(`😔 YOU Lost Your Gane. COMPUTER IS ${computer}`)
-}
-
-else if (you == "S" && computer == "W") {
-  console.log(`☺ YOU WIN. COMPUTER IS ${computer}`)
-}
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
 
 
-else if (you == "G" && computer == "S") {
-  console.log(`☺️ YOU WIN. COMPUTER IS ${computer}`)
-}
+
+    let a = ["S", "G", "W"]
+
+    let computer = a[Math.floor(Math.random() * a.length)]
+
+    let you = e.currentTarget.classList
 
 
-else if (you == "G" && computer == "W") {
-  console.log(`😔 YOU Lost Your Game. COMPUTER IS ${computer}`)
-}
+    if (you.contains("S") && computer == "G") {
+      console.log(`😔 YOU Lost Your Gane. COMPUTER IS ${computer}`)
+    }
+
+    else if (you.contains("S") && computer == "W") {
+      console.log(`☺ YOU WIN. COMPUTER IS ${computer}`)
+    }
 
 
-else if (you == "W" && computer == "S") {
-  console.log(`😔 YOU Lost Your Game. COMPUTER IS ${computer}`)
-}
+    else if (you.contains("G") && computer == "S") {
+      console.log(`☺️ YOU WIN. COMPUTER IS ${computer}`)
+    }
 
-else if (you == "W" && computer == "G") {
-  console.log(`☺ YOU WIN. COMPUTER IS ${computer}`)
-}
 
-else if (you == computer) {
-  console.log(`😲 Game Draw. Because computer Is Also ${computer}`)
-}
+    else if (you.contains("G") && computer == "W") {
+      console.log(`😔 YOU Lost Your Game. COMPUTER IS ${computer}`)
+    }
 
-else {
-  console.log(computer)
-}
+
+    else if (you.contains("W") && computer == "S") {
+      console.log(`😔 YOU Lost Your Game. COMPUTER IS ${computer}`)
+    }
+
+    else if (you.contains("W") && computer == "G") {
+      console.log(`☺ YOU WIN. COMPUTER IS ${computer}`)
+    }
+
+    else if (you.contains("S") == computer) {
+      console.log(`😲 Game Draw. Because computer Is Also ${computer}`)
+    }
+
+    else if (you.contains("G") == computer) {
+      console.log(`😲 Game Draw. Because computer Is Also ${computer}`)
+    }
+
+    else if (you.contains("W") == computer) {
+      console.log(`😲 Game Draw. Because computer Is Also ${computer}`)
+    }
+
+    else {
+      console.log(computer)
+    }
+
+  })
+})
+
+
+
+
+restart.addEventListener("click", ()=>{
+ 
+})
+
+
+
